eeedf12e763af3ed3e724e4a9e09a037dbbdd13e
wundeeh <jakiriksh@gmail.com>
2022-11-28 22:21:05 +0000

    whenever a short link is pressed it redirects you to the longURL that is tied to that link

--- express_server.js
+++ express_server.js
@@ -53,6 +53,11 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars)
 });
 
+app.get("/u/:id", (req, res) => {
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
